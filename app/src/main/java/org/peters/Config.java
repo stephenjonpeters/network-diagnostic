3565b7dbd2af2b011e06a4d4fbea8a79f79eaee2
Steve Peters <25870521+stephenjonpeters@users.noreply.github.com>
2024-04-21 18:38:07 -0600

    gradle test working

--- app/src/main/java/org/peters/Config.java
+++ app/src/main/java/org/peters/Config.java
@@ -1,24 +1,8 @@
 package org.peters;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.File;
-import java.io.IOException;
-
 public class Config {
-    private String url;
-    private String host;
-
-    public Config(){
-        ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File("src/main/resources/config.json");
-        try {
-            Config config = objectMapper.readValue(file, Config.class);
-            this.url = config.getUrl();
-            this.host = config.getHost();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
-    public String getHost(){ return this.host; }
-    public String getUrl(){return this.url;}
+    String hostName;
+    String ip;
+    public String getHostName(){ return this.hostName; }
+    public String getIP(){ return this.ip; }
 }
